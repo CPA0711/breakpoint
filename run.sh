e0a41fbb7690d6858bde04b48bd887058f245c7c
CPA0711 <0711.cpa@gmail.com>
2026-06-26 16:32:17 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-# CPA BREAKPOINT 
+# CPA BREAKPOINT - Cek RPS Limit Google
 
 set -e 
 CSV=breakpoint.csv
 
-# BANNER ASCII
+# BANNER ASCII - Versi Aman No Kutip
 echo "  _______  _____  ______    _______  ______   _______  _______  "
 echo " / ____/ |/ / _ ) / __/   / ____/ / __/ /  /  _/ _ \ / ____/  "
 echo "/ /      |   / _  \ \/    /     / _// /___/ // , _// / __    "
 echo "\____/   |__//____/___/   /_/___/ /___/____/___/_/|_|/___/    "
-echo "        CPA BREAKPOINT 
+echo "        CPA BREAKPOINT - By CPA0711"
 echo ""
 
-echo "🚀 SENDING TEST"
+echo "🚀 JALANIN TEST KE GOOGLE..."
 go run breakpoint.go -n 100 -c 10 -step=10s -warmer=3s -interval=100ms -url=https://google.com -csv=$CSV
 
 echo ""
 echo "✅ SELESAI. CSV ada di: $CSV"
 echo "📌 Liat bagian 'SUMMARY' di atas. Titik mentoknya = Breakpoint"
-"""
